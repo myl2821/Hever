3f846ae470af0762997ca312149f1f17c99eb070
myl <myl2821@gmail.com>
2015-10-16 18:17:41 +0800

    Match HTTP method

--- src/doit.rs
+++ src/doit.rs
@@ -2,14 +2,31 @@ use std::net::TcpStream;
 use std::io::prelude::*;
 use std::io;
 use bufstream::BufStream;
+use std::ascii::AsciiExt;
 
 pub fn handle_client(mut stream: BufStream<TcpStream>) -> Result<(), io::Error> {
     println!("incoming req");
     loop {
         let mut line = String::new();
         try!(stream.read_line(&mut line));
-        print!("{}", line);
+        // get request params
+        let req_vec: Vec<&str> = line.split(' ').collect();
+        if req_vec.len() < 3 {
+            return Ok(());
+        }
+        let method = req_vec[0].to_ascii_uppercase();
+
+        match method.as_ref() {
+            "GET" => {
+            },
+            "HEAD" => {
+            },
+            _ => {
+            }
+        }
+
         try!(stream.write(line.as_bytes()));
         try!(stream.flush());
+        return Ok(());
     }
 }
